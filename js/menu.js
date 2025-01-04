c7625fe7a6bf27f62d1b5bdc0f87a2989256f06d
CronosSon <jordy.branger@hotmail.com>
2025-01-03 22:01:23 -0300

    Alteracoes no menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,9 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('ul').classList.toggle('active');
+// Seleciona o botão de menu e o menu
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('ul');
+
+// Adiciona o evento de clique no botão de menu
+menuToggle.addEventListener('click', () => {
+    // Alterna a classe 'active' para mostrar ou esconder o menu
+    menu.classList.toggle('active');
 });
