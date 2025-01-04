5252f9b14b9df1c5555fff0d5ccd014f5d5632e1
CronosSon <jordy.branger@hotmail.com>
2025-01-03 22:30:26 -0300

    Att

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,13 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('ul');
 
-// Adiciona o evento de clique no botão de menu
-menuToggle.addEventListener('click', () => {
-    // Alterna a classe 'active' para mostrar ou esconder o menu
-    menu.classList.toggle('active');
-});
+// Verifique se o botão e o menu são encontrados
+if (menuToggle && menu) {
+    // Adiciona o evento de clique no botão de menu
+    menuToggle.addEventListener('click', () => {
+        // Alterna a classe 'active' para mostrar ou esconder o menu
+        menu.classList.toggle('active');
+    });
+} else {
+    console.error("Elementos não encontrados!");
+}
